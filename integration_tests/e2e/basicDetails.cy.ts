9aca486654b2cb13cc4ff7e0ab43bbc52cdaeeea
Joseph.Dundon <jdundon@unilink.com>
2025-11-27 09:42:29 +0000

    test fixes

--- integration_tests/e2e/basicDetails.cy.ts
+++ integration_tests/e2e/basicDetails.cy.ts
@@ -1,14 +1,6 @@
 context('Basic Details page', () => {
   it('can see readonly fields', () => {
     cy.visit('/basic-details/00000000-0000-0000-0000-000000000001')
-    cy.get('#page-title').should('contain.text', 'COSSO - Basic Details')
-    cy.get('#id').should('contain.text', '00000000-0000-0000-0000-000000000001')
-  })
-
-  it('can see buttons', () => {
-    cy.visit('/basic-details/00000000-0000-0000-0000-000000000001')
-    cy.get('#continue-button').should('contain.text', 'Continue')
-    cy.get('#close-button').should('contain.text', 'Save Progress and Close')
-    cy.get('#refresh-from-ndelius--button').should('contain.text', 'Refresh from Delius')
+    cy.get('#crn').should('contain.text', 'X012345')
   })
 })
