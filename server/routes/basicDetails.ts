6459f024ec3fe004b88915ed559ed3604fef6f55
Joseph.Dundon <jdundon@unilink.com>
2025-11-27 09:28:35 +0000

    Simplify initial PR

--- server/routes/basicDetails.ts
+++ server/routes/basicDetails.ts
@@ -3,7 +3,6 @@ import { AuthenticationClient } from '@ministryofjustice/hmpps-auth-clients'
 import AuditService, { Page } from '../services/auditService'
 
 import CossoApiClient, { Cosso } from '../data/cossoApiClient'
-import { BasicDetails } from '../data/ndeliusIntegrationApiClient'
 
 export default function basicDetailsRoutes(
   router: Router,
@@ -15,28 +14,14 @@ export default function basicDetailsRoutes(
   router.get('/basic-details/:id', async (req, res) => {
     await auditService.logPageView(Page.BASIC_DETAILS, { who: res.locals.user.username, correlationId: req.id })
     const cossoClient = new CossoApiClient(authenticationClient)
-
     const cossoId: string = req.params.id
-    const basicDetails: BasicDetails = null
-
     const cosso: Cosso = await cossoClient.getCossoById(cossoId, res.locals.user.username)
 
     res.render('pages/basic-details', {
       cosso,
       cossoId,
-      basicDetails,
       currentPage,
     })
-  })
-
-  router.post('/basic-details/:id', async (req, res) => {
-    if (req.body.action === 'saveProgressAndClose') {
-      res.send(
-        `<p>You can now safely close this window</p><script nonce="${res.locals.cspNonce}">window.close()</script>`,
-      )
-    } else {
-      res.redirect(`/basic-details/${req.params.id}`)
-    }
   })
   return router
 }
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,14 +1,12 @@
 import { Router } from 'express'
 
 import type { Services } from '../services'
-import { Page } from '../services/auditService'
 import basicDetailsRoutes from './basicDetails'
 
 export default function routes({ auditService, hmppsAuthClient }: Services): Router {
   const router = Router()
 
   router.get('/', async (req, res, next) => {
-    await auditService.logPageView(Page.EXAMPLE_PAGE, { who: res.locals.user.username, correlationId: req.id })
     res.render('pages/index')
   })
 
