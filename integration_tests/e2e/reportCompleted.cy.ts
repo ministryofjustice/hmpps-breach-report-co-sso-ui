4c41c45eae95b594d895cb974b2a95a540e3fc58
Joseph.Dundon <jdundon@unilink.com>
2025-12-23 16:23:02 +0000

    test fix

--- integration_tests/e2e/reportCompleted.cy.ts
+++ integration_tests/e2e/reportCompleted.cy.ts
@@ -2,6 +2,5 @@ context('Report completed', () => {
   it('Basic Details not populated', () => {
     cy.visit('/report-completed/00000000-0000-0000-0000-000000000011')
     cy.get('#reviewReport').should('exist')
-    cy.get('#close-window').should('exist')
   })
 })
