414464361f0a48a30dd449fcf7040930c4b1857c
James MacBeth <56261600+jamesmacbeth-unilink@users.noreply.github.com>
2026-06-19 12:03:44 +0100

    COSSO-111 - Display monetary value additional sentences (#78)
    * COSSO-111 - Display monetary value additional sentences
    
    * COSSO-111 - Test fix
    
    * COSSO-111 - Test fix

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -41,4 +41,5 @@ export default function nunjucksSetup(app: express.Express): void {
   njkEnv.addFilter('initialiseName', initialiseName)
   njkEnv.addFilter('assetMap', (url: string) => assetManifest[url] || url)
   njkEnv.addFilter('toUserDate', toUserDate)
+  njkEnv.addFilter('toGBP', (amount: number) => Number(amount).toFixed(2))
 }
