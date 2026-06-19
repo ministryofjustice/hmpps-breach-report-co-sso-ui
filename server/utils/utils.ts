2d2d6cbf296741c585f49dbe0bbe93422fdf29bf
James MacBeth <56261600+jamesmacbeth-unilink@users.noreply.github.com>
2026-06-19 12:03:58 +0100

    COSSO-114 - Fix change link not showing on Compliance section (#79)
    * COSSO-114 - Fix change link not showing on Compliance section
    
    * COSSO-114 - Fix missing declaration

--- server/utils/utils.ts
+++ server/utils/utils.ts
@@ -164,6 +164,7 @@ export function createBlankCossoWithId(id: string): Cosso {
     signAndSendSaved: false,
     sheetSentBy: '',
     signedByRo: false,
+    complianceToDateSaved: false,
   }
 }
 
