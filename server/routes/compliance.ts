8b01aaea524143cde75c7d06251e53c1645d80ec
james.macbeth <james.macbeth@justice.gov.uk>
2026-06-29 16:40:10 +0100

    COSSO-152 - Fix linting

--- server/routes/compliance.ts
+++ server/routes/compliance.ts
@@ -282,7 +282,7 @@ export default function complianceRoutes(
         }
 
         if (notes && notes.length > 20000) {
-          errorMessages['requirement_' + uiId] = {
+          errorMessages[`requirement_${uiId}`] = {
             text: 'Requirement Notes: This field must be 20000 characters or less',
           }
         }
