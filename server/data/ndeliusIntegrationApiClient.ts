3ccf737ad28c9a2eb92c3fc1a87bd80e1ef84a3f
Paul Miller <paul.miller4@justice.gov.uk>
2026-08-03 16:53:57 +0100

    Cosso-188 7B: Breach Report CO SSO - Populating PDF - Requirements Length Missing (#145)
    * COSSO-188: rqmnts missing length
    
    * COSSO-188: Test fix
    
    ---------
    
    Co-authored-by: rmccormack <rmccormack@unilink.com>
    Co-authored-by: James MacBeth <56261600+jamesmacbeth-unilink@users.noreply.github.com>

--- server/data/ndeliusIntegrationApiClient.ts
+++ server/data/ndeliusIntegrationApiClient.ts
@@ -186,14 +186,8 @@ export interface WitnessDetails {
   replyAddresses: DeliusAddress[]
 }
 
-export interface Requirement {
-  id: number
-  type: ReferenceData
-  subType: ReferenceData
-}
-
 export interface Requirements {
-  requirements: Requirement[]
+  requirements: DeliusRequirement[]
   breachReasons: ReferenceData[]
 }
 
