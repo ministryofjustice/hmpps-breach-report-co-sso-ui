03b0e3f8d4fe750c84893d6237c92f0f23b6c161
peter wilson <pwilson@unilink.com>
2026-08-25 17:29:35 +0100

    updated allowedlist

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -9,5 +9,6 @@ export default configureAllowedScripts({
     'node_modules/fsevents@2.3.3': 'ALLOW',
     'node_modules/playwright/node_modules/fsevents@2.3.2': 'ALLOW',
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
+    'node_modules/protobufjs@7.6.5': 'ALLOW',
   },
 })
