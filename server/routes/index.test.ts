1d366f02f0db5f49066f250cecc1f5fe988e708c
peter wilson <pwilson@unilink.com>
2026-09-14 16:08:11 +0100

    Page heading fixes

--- server/routes/index.test.ts
+++ server/routes/index.test.ts
@@ -31,7 +31,7 @@ describe('GET /', () => {
       .expect('Content-Type', /html/)
       .expect(200)
       .expect(res => {
-        expect(res.text).toContain('This site is under construction...')
+        expect(res.text).toContain('Incorrect Navigation Route')
       })
   })
 })
